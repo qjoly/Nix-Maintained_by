9161cd67dffe83a84d2d1aa86f3065e6f01830a7
Quentin JOLY <82603435+QJoly@users.noreply.github.com>
2023-05-10 11:14:08 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 sh -c "python3 /data/main.py $*"
+git add $1
+git config user.email 41898282+github-actions[bot]@users.noreply.github.com
+git config user.name "GitHub Actions[Bot]"
+# Push only if README.md has been modified
+if [[ $(git status --porcelain "$1" | wc -l) -gt 0 ]]; then git commit -m "[BOT] Update $1"; git push; fi
