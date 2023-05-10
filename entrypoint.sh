07055120641037402e64effddc680f77812d9ddf
Quentin JOLY <82603435+QJoly@users.noreply.github.com>
2023-05-10 12:03:46 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 echo 'Starting Python script'
 sh -c "python3 /data/main.py $*"
-git add $1
 git config --global --add safe.directory /github/workspace
+
 git config --global user.email 41898282+github-actions[bot]@users.noreply.github.com
 git config --global user.name "GitHub Actions[Bot]"
+git add $1
+
 # Push only if README.md has been modified
 if [[ $(git status --porcelain "$1" | wc -l) -gt 0 ]]; then git commit -m "[BOT] Update $1"; git push; fi
